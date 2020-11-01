4c1909bc9cf4628d905b3ac01f7fd924db45b037
cisfor50 <camilagauna@outlook.com>
2020-10-31 21:28:33 -0300

    adding mejorDisparo() and promedioDisparo()

--- calculos_disparo.py
+++ calculos_disparo.py
@@ -5,7 +5,20 @@ def pitagoras(x, y):
   eje_x = (int(x) * int(y))
   eje_y = (int(x) * int(y))
   resultado = sqrt(eje_x + eje_y)
+  resultado = round(resultado, 2)
   return resultado
 
-def mejor_disparo():
-  
+def mejorDisparo(a, b, c):
+  temp = 0
+  if a < b and a < c:
+    temp = a
+  elif b < c:
+    temp = b
+  else:
+    temp = c
+  return temp
+
+def promedioDisparo(x,y,z):
+  resultado = (x + y + z) /3
+  resultado = round(resultado, 2)
+  return resultado
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from calculos_disparo import pitagoras
+from calculos_disparo import pitagoras, mejorDisparo, promedioDisparo
 
 participantes = []
 numero_id = ''
-
-while numero_id != 999:
+while numero_id != '999':
   numero_id = input("Ingrese el ID del participante: ")
-  nombre_completo = input("Ingrese nombre y apellido del participante: ")
+  nombre = input("Ingrese nombre del participante: ")
+  apellido = input("Ingrese apellido del participante: ")
   edad = int(input("Ingrese la edad del participante: "))
   sexo = input("Ingrese el sexo del participante (F/M): ")
   disparos = []
@@ -13,6 +13,23 @@ while numero_id != 999:
     disparo = [input("Disparo. Eje x: "), input("Disparo. Eje y: ")]
     disparo = pitagoras(disparo[0], disparo[1])
     disparos.append(disparo)
-  
-    print(disparos) 
-  
+  mejor_disparo = mejorDisparo(disparos[0], disparos[1], disparos[2])
+  promedio_disparo = promedioDisparo(disparos[0], disparos[1], disparos[2])
+  participante = {"numero_id": '',
+                  "nombre": '',
+                  "apellido": '',
+                  "edad": '',
+                  "sexo": '',
+                  "disparos": '',
+                  "mejor_disparo": '',
+                  "promedio_disparo": ''
+                  }
+  participante['numero_id'] = numero_id
+  participante['nombre'] = nombre
+  participante['apellido'] = apellido
+  participante['edad'] = edad
+  participante['sexo'] = sexo
+  participante['disparos'] = disparos
+  participante['mejor_disparo'] = mejor_disparo
+  participante['promedio_disparo'] = promedio_disparo
+  participantes.append(participante)
